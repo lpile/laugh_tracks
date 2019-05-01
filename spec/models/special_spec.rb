4eda64066d396a11713667d3ec1931905fd0c37c
lpile <logan.pile@gmail.com>
2019-05-01 12:53:49 -0600

    Update Special Model

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -4,6 +4,5 @@ describe Special, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:running_time)}
-    it {should validate_presence_of(:comedian_id)}
   end
 end
