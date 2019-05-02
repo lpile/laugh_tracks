c30624bfe379544e3f6987c86faeb3ebc606b4e5
lpile <logan.pile@gmail.com>
2019-05-02 16:41:57 -0600

    Add statistics test

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe "As a visitor" do
 
       visit "/comedians"
 
-      expect(Special.special_count).to eq(3)
+      expect(comedian.specials_count).to eq(3)
     end
   end
 
@@ -91,6 +91,32 @@ RSpec.describe "As a visitor" do
       visit "/comedians"
 
       expect(page).to have_link('', href: '/comedians/new')
+    end
+  end
+
+  describe "I visit `/comedians`" do
+    describe "I see an area at the top of the page called 'Statistics'" do
+      it "which shows the average age and unique cities" do
+        comedian_1 = Comedian.create!(name: "Jim Gaffigan", age: 52, birthplace: "Elgin, IL", image_url: "https://media.timeout.com/images/103872088/1372/772/image.jpg")
+        comedian_2 = Comedian.create!(name: "Patton Oswalt", age: 50, birthplace: "Portsmouth, VA", image_url: "https://media.vanityfair.com/photos/592efe8516315d40a9678347/master/w_1536,c_limit/patton-oswalt-interview.jpg")
+        comedian_3 = Comedian.create!(name: "Tim Gaffigan", age: 22, birthplace: "Elgin, IL", image_url: "https://media.timeout.com/images/103872088/1372/772/image.jpg")
+        special_1 = comedian_1.specials.create(name: "Obsessed", running_time: 60)
+        special_2 = comedian_1.specials.create(name: "Cinco", running_time: 73)
+        special_3 = comedian_2.specials.create(name: "Noble Ape", running_time: 90)
+        special_4 = comedian_2.specials.create(name: "Werewolves and Lollipops", running_time: 58)
+        special_5 = comedian_3.specials.create(name: "Finest Hour", running_time: 76)
+        special_6 = comedian_3.specials.create(name: "Tragedy Plus Comedy Equals Time", running_time: 59)
+
+        visit "/comedians"
+
+        expect(page).to have_content(comedian_1.name)
+        expect(page).to have_content(comedian_2.age)
+        expect(page).to have_content(comedian_3.birthplace)
+        expect(page).to have_content("Statistics")
+        expect(page).to have_content("Average age")
+        expect(comedian.average_age).to eq(41.3)
+        expect(comedian.uniq_locations).to eq(["Elgin, IL", "Portsmouth, VA"])
+      end
     end
   end
 end
