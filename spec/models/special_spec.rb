9a4fac647d599c36d85dc68b1d25869827eaa89e
lpile <logan.pile@gmail.com>
2019-05-01 18:23:47 -0600

    Add specials count test

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -5,4 +5,19 @@ describe Special, type: :model do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:running_time)}
   end
+
+  describe 'relationships' do
+    it {should belong_to :comedian}
+  end
+
+  describe 'class methods' do
+    it '.special_count' do
+      comedian = Comedian.create!(name: "Jim Gaffigan", age: 52, birthplace: "Elgin, IL", image_url: "https://media.timeout.com/images/103872088/1372/772/image.jpg")
+      special_1 = comedian.specials.create(name: "Obsessed", running_time: 60)
+      special_2 = comedian.specials.create(name: "Cinco", running_time: 73)
+      special_3 = comedian.specials.create(name: "Noble Ape", running_time: 90)
+
+      expect(Special.special_count).to eq(3)
+    end
+  end
 end
